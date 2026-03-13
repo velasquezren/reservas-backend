9637659c391bb81591a40b6a5e68122c7e5e94c3
unknown <httprene2021@gmail.com>
2026-03-13 18:59:12 -0400

    Allow destructive commands to run in production for demo staging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -52,9 +52,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Date::use(CarbonImmutable::class);
 
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
+        // DB::prohibitDestructiveCommands(
+        //     app()->isProduction(),
+        // );
 
         Password::defaults(fn (): ?Password => app()->isProduction()
             ? Password::min(12)
